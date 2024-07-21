66ad82ebd5460dad1ae2fc0d128f6f320e3828ce
Shivam Kumar <160920667+shivam19431@users.noreply.github.com>
2024-07-21 09:50:41 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,70 @@
+public class SudokuSolver {
+    private int[][] board;
+    private static final int SIZE = 9;
+
+    public SudokuSolver(int[][] board) {
+        this.board = board;
+    }
+    private boolean isValid(int row, int col, int num) {
+        // Check row
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 subgrid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    public boolean solve() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) { // Empty cell
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(row, col, num)) {
+                            board[row][col] = num;
+                            if (solve()) {
+                                return true;
+                            }
+                            board[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    public void printBoard() {
+        for (int r = 0; r < SIZE; r++) {
+            for (int d = 0; d < SIZE; d++) {
+                System.out.print(board[r][d]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+
+            if ((r + 1) % 3 == 0) {
+                System.out.print("");
+            }
+        }
+    }
+
+}
